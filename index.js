0088d33e43178ea1a4a367f689a58f501d68cb59
Harum Helmy <harumendhah@gmail.com>
2019-08-25 13:11:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(Math.max(drivers.length-2,1))
+    // drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+// practice the anon function syntax: 
+// const createFareMultiplier = function(multiplier){
+//     return function(fare){
+//         return multiplier * fare
+//     }
+// }
+
+function createFareMultiplier(multiplier){
+    return function(fare){
+        return multiplier * fare
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function(arrayOfDrivers, selectingDrivers){
+    return selectingDrivers(arrayOfDrivers)
+}
